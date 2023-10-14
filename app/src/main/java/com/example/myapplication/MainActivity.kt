590c91eb2744876cb3e79e9749ca57a58057d6b7
nurasan <210103229@stu.sdu.edu.kz>
2023-10-14 13:30:22 +0600

    UI VERSION CARD

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,65 +1,83 @@
-package com.example.myapplication
-
-import android.content.Context
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme(){
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = Color.White){
+            MyApplicationTheme() {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = Color.White
+                ) {
                     BusinessCard()
                 }
             }
-
         }
     }
 }
-
-
-
+@Preview
 @Composable
-fun BusinessCard(){
+fun BusinessCard() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Asko Van", fontSize = 20.sp)
-        
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = "Boss company", fontSize = 16.sp)
-        
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = "Email: nurisASKO@gmail", fontSize = 12.sp)
 
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = "Phone: 123456789", fontSize = 12.sp)
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(100.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Text Composables
+        Text(
+            text = "John Doe",
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Text(
+            text = "CEO at My Company",
+            style = MaterialTheme.typography.bodyMedium,
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Phone, contentDescription = "Phone Icon")
+            Text(
+                text = "123-456-7890",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
     }
 }
